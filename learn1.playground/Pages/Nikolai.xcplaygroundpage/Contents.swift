4aa0c0e6d8fac4b3570f9287644b5771967a1ecd
danya <danya>
2025-10-30 21:13:17 +0300

    сделал задание + файл с курса

---
+++ learn1.playground/Pages/Nikolai.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//Коля первое задание
+
+import Foundation
+
+//one
+var firstHello = "Hello, Skillbox"
+print (firstHello)
+
+//two
+let twoHello = "Hello, Skillbox"
+print (twoHello)
+
+//three
+var threeNumber: Double = 0.5
+print (threeNumber)
+
+//four
+print(10+threeNumber)
---
+++ learn1.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+let x = 111 // константа
+var y = 222 // переменная
+
+
+let alertMessage = "Слишком много попыток"
+
+var defaultErrorMessage = "Ошибочка"
+
+defaultErrorMessage = alertMessage
+
+
+y*=10
+
+
+//типы
+let first: Int = 32
+var second: String = "Stroka"
+var double: Double = 2.233
+
+
+let string2: String = String(first)
+var textToSnow = "Значение переменное int равно \(first)"
+
+textToSnow = #"Тратата, чтобы выветси текст, \(int) в итоге получим \#(second)"#
+
+let stix = """
+Я вас любил: любовь ещё, быть может,
+В душе моей угасла не совсем;
+Но пусть она вас больше не тревожит;
+Я не хочу печалить вас ничем.
+Я вас любил безмолвно, безнадежно,
+То робостью, то ревностью томим;
+Я вас любил так искренно, так нежно,
+Как дай вам Бог любимой быть другим
+"""
