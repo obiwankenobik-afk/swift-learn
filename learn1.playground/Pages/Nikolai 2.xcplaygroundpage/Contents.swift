35e0d5bc560ad6b6a414df13adc256a722fbbc8a
danya <danya>
2025-10-31 16:19:48 +0300

    исправленные ошибки во второй

--- learn1.playground/Pages/Nikolai 2.xcplaygroundpage/Contents.swift
+++ learn1.playground/Pages/Nikolai 2.xcplaygroundpage/Contents.swift
@@ -1,15 +1,15 @@
 // Задание 1. Дополните функцию nextNumber() так, чтобы она получала целое число и возвращала число, следующее за ним по порядку.
 
 func nextNumber(number: Int) -> Int {
-    return number + 1
+    number + 1
 }
 nextNumber(number:5)
 
 
 // Задание 2. В предыдущем задании, вы вызвали существующую функцию. В этом задании требуется сделать обратное, т.е. вам нужно по описанию вызова функции ее создать. Дан вызов функции. Превратите его в функцию, которая вызывается так: getSquare(number: 3) — и возвращает квадрат целого числа, в данном случае 9.
 
-func getSquare(number: Int) -> Int{
-    return number * number
+func getSquare(number: Int) -> Int {
+    number * number
 }
 
 let value = getSquare(number: 3)
@@ -17,16 +17,16 @@ print(value)
 
 // Задание 3. Напишите функцию, которая получает на вход количество секунд и возвращает количество минут и оставшихся секунд. Пример: в качестве аргумента подаётся 70 секунд, тогда функция возвращает два целых числа. Первое — 1 минуту, второе — 10 секунд.
 
-func secondReturn(_ second:Int) -> (Int, Int) {
-    return (second / 60, second % 60)
+func secondReturn(_ second: Int) -> (min: Int, sec: Int) {
+    (second / 60, second % 60)
 }
 
 let countSecond = secondReturn(70)
-print("Количество минут \(countSecond.0), количество секунд \(countSecond.1)")
+print("Количество минут \(countSecond.min), количество секунд \(countSecond.sec)")
 
 // Задание 4. Напишите функцию, которая будет конкатенировать (объединять) две строки в одну. Вызовите вашу функцию и сохраните результат в переменную. Выведите результат в консоль. Для проверки используйте константы ниже.
 
-func linePlus (_ line1: String, _ line2: String) -> String {
+func linePlus(_ line1: String, _ line2: String) -> String {
     return line1 + line2
 }
 
